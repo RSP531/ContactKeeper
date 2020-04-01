96ecaa65bedec2a6bb3da81d555930f8975f5cf7
Rob Peschke <rspesc31@gmail.com>
2020-04-01 14:08:50 -0400

    setting up basic functionality for user and copy/pasted to contacts and auth to remove errros

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const port = process.env.port || 5000;
 
 app.get("/", (req, res) => res.json({ msg: "WELCOME TO THE API bi-otch" }));
 
+app.use("/api/users", require("./routes/users"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/contacts", require("./routes/contacts"));
+
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
 );
