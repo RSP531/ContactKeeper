78e9ad52bfc9141cc3269ee2d0b8a7f508adfbf6
Rob Peschke <rspesc31@gmail.com>
2020-04-01 16:59:39 -0400

    forgot to have User defined in auth

--- routes/auth.js
+++ routes/auth.js
@@ -5,10 +5,19 @@ const jwt = require("jsonwebtoken");
 const config = require("config");
 const { check, validationResult } = require("express-validator");
 
+const User = require("./models/User");
+
 // @route   GET api/auth
 // @desc    Get logged in user
 // @access  Private
-router.get(
+router.get("/", (req, res) => {
+  res.send("Logged in user");
+});
+
+// @route   POST api/auth
+// @desc    Auth user & get token
+// @access  Public
+router.post(
   "/",
   [
     check("email", "Please include a valid email").isEmail(),
@@ -46,18 +55,12 @@ router.get(
           res.json({ token });
         }
       );
-    } catch (err) {}
-    // res.send("Get logged in user");
-    console.error(err.message);
-    res.status(500).send("Server Error");
+    } catch (err) {
+      // res.send("Get logged in user");
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
   }
 );
-
-// @route   POST api/auth
-// @desc    Auth user & get token
-// @access  Public
-router.post("/", (req, res) => {
-  res.send("Log in user");
-});
 
 module.exports = router;
