1be4d9308b29d9ba92ba38f4e105b90693e01dc8
Rob Peschke <rspesc31@gmail.com>
2020-04-01 15:16:05 -0400

    finished users"

--- routes/users.js
+++ routes/users.js
@@ -18,11 +18,16 @@ router.post(
       .isEmpty(),
     check("email", "Please include a valid email").isEmail(),
     check(
-      "password, Please enter a password with 6 or more characters"
+      "password",
+      "Please enter a password with 6 or more characters"
     ).isLength({ min: 6 })
   ],
   (req, res) => {
-    res.send(req.body);
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send("passed");
   }
 );
 
