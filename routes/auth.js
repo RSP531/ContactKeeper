eee3d4abafe7c51b0033f199015e00f6a2a5144d
Rob Peschke <rspesc31@gmail.com>
2020-04-01 15:54:37 -0400

    added auth validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,18 +1,27 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const { check, validationResult } = require("express-validator");
 
 // @route   GET api/auth
 // @desc    Get logged in user
 // @access  Private
-
-router.get("/", (req, res) => {
-  res.send("Get logged in user");
-});
+router.get(
+  "/",
+  [
+    check("email", "Please include a valid email").isEmail(),
+    check("password", "Password is required").exists()
+  ],
+  (req, res) => {
+    res.send("Get logged in user");
+  }
+);
 
 // @route   POST api/auth
 // @desc    Auth user & get token
 // @access  Public
-
 router.post("/", (req, res) => {
   res.send("Log in user");
 });
