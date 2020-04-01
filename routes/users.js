25d0292fb76649befb5ab8b26efa1d59e155f8ee
Rob Peschke <rspesc31@gmail.com>
2020-04-01 15:23:52 -0400

    adding bcrypt to users route

--- routes/users.js
+++ routes/users.js
@@ -22,12 +22,35 @@ router.post(
       "Please enter a password with 6 or more characters"
     ).isLength({ min: 6 })
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send("passed");
+    const { firstName, lastName, email, password } = req.body;
+
+    try {
+      let user = await user.findOne({ email });
+      if (user) {
+        return res.status(400).json({ msg: "User already exists" });
+      }
+      user = new user({
+        firstName,
+        lastName,
+        email,
+        password
+      });
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      res.send("User saved");
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
   }
 );
 
