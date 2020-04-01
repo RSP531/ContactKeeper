12128a106287a8f39588574775adffeee0dd7167
Rob Peschke <rspesc31@gmail.com>
2020-04-01 16:03:47 -0400

    finishing the auth but not fully complete

--- routes/auth.js
+++ routes/auth.js
@@ -14,8 +14,42 @@ router.get(
     check("email", "Please include a valid email").isEmail(),
     check("password", "Password is required").exists()
   ],
-  (req, res) => {
-    res.send("Get logged in user");
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res.status(400).json({ msg: "Invalid credentials" });
+      }
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res.status(400).json({ msg: "Invalid credentials" });
+      }
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        {
+          expiresIn: 360000
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {}
+    // res.send("Get logged in user");
+    console.error(err.message);
+    res.status(500).send("Server Error");
   }
 );
 
