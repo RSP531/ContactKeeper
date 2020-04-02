aba2585fe04f651ef267672d96817a27f5e77947
Rob Peschke <rspesc31@gmail.com>
2020-04-02 13:26:10 -0400

    created the contact item component for each of the contacts and rendering to the page. starting to work on the contact form

---
+++ client/src/components/contacts/ContactItem.js
@@ -0,0 +1,45 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const ContactItem = ({
+  contact: { id, lastName, firstName, email, phone, type }
+}) => {
+  return (
+    <div className="card bg-light">
+      <h3 className="text-primary text-left">
+        {firstName} {lastName}{" "}
+        <span
+          style={{ float: "right" }}
+          className={
+            "badge " +
+            (type === "Professional" ? "badge-success" : "badge-primary")
+          }
+        >
+          {type.charAt(0).toUpperCase() + type.slice(1)}
+        </span>
+      </h3>
+      <ul className="list">
+        {email && (
+          <li>
+            <i className="fas fa-envelope-open" /> {email}
+          </li>
+        )}
+        {phone && (
+          <li>
+            <i className="fas fa-phone" /> {phone}
+          </li>
+        )}
+        <p>
+          <button className="btn btn-dark btn-sm">Edit</button>
+          <button className="btn btn-danger btn-sm">Delete</button>
+        </p>
+      </ul>
+    </div>
+  );
+};
+
+ContactItem.propTypes = {
+  contact: PropTypes.object.isRequired
+};
+
+export default ContactItem;
