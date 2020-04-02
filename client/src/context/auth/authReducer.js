d0b27d6e46e959523909ae0462d3b5d69807b8f1
Rob Peschke <rspesc31@gmail.com>
2020-04-02 16:19:07 -0400

    added to the auth reducer

---
+++ client/src/context/auth/authReducer.js
@@ -0,0 +1,52 @@
+import React, { useReducer } from "react";
+import AuthContext from "./contactContext";
+import authReducer from "./contactReducer";
+import {
+  REMOVE_ALERT,
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  LOGOUT,
+  LEAR_ERRORS
+} from "../types/types";
+
+const AuthState = props => {
+  const initialState = {
+    token: localStorage.getItem("token"),
+    isAuthenticated: null,
+    loading: true,
+    user: null,
+    error: null
+  };
+
+  const [state, dispatch] = useReducer(contactReducer, initialState);
+
+  // Load User
+
+  // Register User
+
+  // Login User
+
+  // Logout
+
+  //Clear Errors
+
+  return (
+    <AuthContext.Provider
+      value={{
+        token: state.token,
+        isAuthenticated: state.isAuthenticated,
+        loading: state.loading,
+        user: state.user,
+        error: state.error
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthState;
