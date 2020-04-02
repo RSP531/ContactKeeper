70fbf003e882d1b7042d08aafb4f57b1701d886e
Rob Peschke <rspesc31@gmail.com>
2020-04-02 11:36:23 -0400

    setting up basic app functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
+import About from "./components/pages/About";
+import Home from "./components/pages/Home";
 
 const App = () => {
   return (
-    <div className="App">
-      <Navbar />
-      Rob's App
-    </div>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
+          </Switch>
+        </div>
+      </Fragment>
+    </Router>
   );
 };
 
