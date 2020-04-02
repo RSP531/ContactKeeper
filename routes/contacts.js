710310e414d777f9e719f9f82dc5ccbc5965b88b
Rob Peschke <rspesc31@gmail.com>
2020-04-02 10:22:59 -0400

    updated the put or change a contact route

--- routes/contacts.js
+++ routes/contacts.js
@@ -70,8 +70,38 @@ router.post(
 // @route   PUT api/contacts/:id
 // @desc    Change existing contact
 // @access  Private
-router.put("/:id", (req, res) => {
-  res.send(`Change a contact with number ${req.params.id}`);
+router.put("/:id", auth, async (req, res) => {
+  const { firstName, lastName, email, phone, type } = req.body;
+
+  //  Build a contact object to see if submitted
+  const contactFields = {};
+  if (firstName) contactFields.firstName = firstName;
+  if (lastName) contactFields.lastName = lastName;
+  if (email) contactFields.email = email;
+  if (phone) contactFields.phone = phone;
+  if (type) contactFields.type = type;
+  try {
+    let contact = await Contact.findById(req.params.id);
+    if (!contact) {
+      return res.status(404).json({ msg: "Contact does not exist" });
+    }
+    //Make sure the current user owns the contact
+
+    if (contact.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "Not Authorized" });
+    }
+
+    contact = await Contact.findByIdAndUpdate(
+      req.params.id,
+      { $set: contactFields },
+      { new: true }
+    );
+    res.json({ contact });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+  //res.send(`Change a contact with number ${req.params.id}`);
 });
 
 // @route   DELETE api/contacts/:id
