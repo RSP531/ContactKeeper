31edd05b28eb4e12cd474d9af6438a82f9c945e9
Rob Peschke <rspesc31@gmail.com>
2020-04-03 10:02:16 -0400

    finishing registering the user and adding the redirect to home

--- client/src/context/auth/authReducer.js
+++ client/src/context/auth/authReducer.js
@@ -11,6 +11,13 @@ import {
 
 export default (state, action) => {
   switch (action.type) {
+    case USER_LOADED:
+      return {
+        ...state,
+        isAuthenicated: true,
+        loading: false,
+        user: action.payload
+      };
     case REGISTER_SUCCESS:
       localStorage.setItem("token", action.payload.token);
       return {
@@ -20,6 +27,7 @@ export default (state, action) => {
         loading: false
       };
     case REGISTER_FAIL:
+    case AUTH_ERROR:
       localStorage.removeItem("token");
       return {
         ...state,
@@ -28,14 +36,6 @@ export default (state, action) => {
         loading: false,
         user: null,
         error: action.payload
-      };
-    case USER_LOADED:
-      return {
-        ...state
-      };
-    case AUTH_ERROR:
-      return {
-        ...state
       };
     case LOGIN_SUCCESS:
       return {
---
+++ client/src/utils/setAuthToken.js
@@ -0,0 +1,14 @@
+//check to see if token is passed in
+//if so, set to the global header
+//if not, delete from the global header
+import axios from "axios";
+
+const setAuthToken = token => {
+  if (token) {
+    axios.defaults.headers.common["x-auth-token"] = token;
+  } else {
+    delete axios.defaults.headers.common["x-auth-token"];
+  }
+};
+
+export default setAuthToken;
