116d60c518d34cd93b9ab9a513071d32c729e281
Rob Peschke <rspesc31@gmail.com>
2020-04-03 10:42:30 -0400

    fixed the login success and the login fail in the reducers

--- client/src/context/auth/authReducer.js
+++ client/src/context/auth/authReducer.js
@@ -19,6 +19,7 @@ export default (state, action) => {
         user: action.payload
       };
     case REGISTER_SUCCESS:
+    case LOGIN_SUCCESS:
       localStorage.setItem("token", action.payload.token);
       return {
         ...state,
@@ -28,6 +29,7 @@ export default (state, action) => {
       };
     case REGISTER_FAIL:
     case AUTH_ERROR:
+    case LOGIN_FAIL:
       localStorage.removeItem("token");
       return {
         ...state,
@@ -36,14 +38,6 @@ export default (state, action) => {
         loading: false,
         user: null,
         error: action.payload
-      };
-    case LOGIN_SUCCESS:
-      return {
-        ...state
-      };
-    case LOGIN_FAIL:
-      return {
-        ...state
       };
     case LOGOUT:
       return {
