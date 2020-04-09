00c672adf0cfa1de1040a9459f482e31498eac1e
Rob Peschke <rspesc31@gmail.com>
2020-04-09 17:29:12 -0400

    still broken

--- routes/models/Item.js
+++ routes/models/Item.js
@@ -1,13 +1,19 @@
 const mongoose = require("mongoose");
 
 const ItemSchema = mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "users"
+  },
   item: {
     type: String,
-    required: true
+    required: true,
+    default: "Grocery Item"
   },
   list: {
     type: String,
-    required: true
+    required: true,
+    default: "1"
   }
 });
 
